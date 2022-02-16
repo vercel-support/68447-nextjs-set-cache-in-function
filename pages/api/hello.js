803a6bb22a2508f81f1c3fcf4b9eda174bf9c299
William Li <email@williamli.dev>
2022-02-16 14:51:54 +0800

    setting cache-control header inside serverless function

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.set('Cache-control', 'public, max-age=0, s-maxage=60, immutable, stale-while-revalidate')
+  res.setHeader('Cache-control', 'public, max-age=0, s-maxage=60, immutable, stale-while-revalidate')
   res.status(200).json({ name: 'John Doe', now: Date.now() })
 }
